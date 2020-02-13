c311832b78f1b02bca6d680c12b1c516897d867b
spyro21 <38793743+spyro21@users.noreply.github.com>
2020-02-13 10:44:00 -0600

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "My name is Jack Legate\n";
+  std::cout << "I am a CS High School Student\n";
+  std::cout << "My Hobbies are: \n";
+  std::cout << "\t coding\n";
+  std::cout << "\t gaming\n";
+  std::cout << "\t watching e-sports\n";
+  std::cout << "\t jamming to Cotton Eye Joe\n";
+  std::cout << "goodbye!\n";
+  
+}
